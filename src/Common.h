31a095b5421f459fda6fda36f8e74953244026f9
Matt Stockwell <matt.stockwell@lifx.co>
2019-08-04 22:09:10 +1000

    Add LogHexDump

--- src/Common.h
+++ src/Common.h
@@ -2,6 +2,7 @@
 #define ATARI_VCS_EMU_COMMON_H
 
 #include <cstdint>
+#include "HexDump.h"
 
 #define SafeDelete(pointer) if(pointer != NULL) {delete pointer; pointer = NULL;}
 #define SafeDeleteArray(pointer) if(pointer != NULL) {delete [] pointer; pointer = NULL;}
@@ -9,9 +10,11 @@
 #define DEBUG
 
 #ifdef DEBUG
-#define Log(M, ...) (printf(M "\n", ##__VA_ARGS__))
+#define Log(M, ...) printf(M "\n", ##__VA_ARGS__)
+#define LogHexDump(...) hexDump(__VA_ARGS__)
 #else
-#define Log(msg, ...)
+#define Log(msg, ...) (void)0
+#define LogHexDump(...) (void)0
 #endif
 
 struct CRGBA {
