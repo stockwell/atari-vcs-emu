90eca66904f804a50b97b6a8909237a035dc64a9
Matt Stockwell <matt.stockwell@lifx.co>
2019-08-07 22:26:25 +1000

    Test: Fix up CMakeLists, gearing up for testing opcodes. Will need to add some methods to allow for inspection, or consider making registers public scope as it's a black box currently

--- test/OPCodeTest.cpp
+++ test/OPCodeTest.cpp
@@ -3,9 +3,34 @@
 *****************************************************************************/
 #include "gtest/gtest.h"
 
+#include "MOS6502Core.h"
+#include "Memory.h"
+
 /*****************************************************************************
 * Unit Tests
 *****************************************************************************/
-TEST(DUMMY, DummyTest) {
-  ASSERT_EQ(1, 1);
+
+class MOS6502Test : public ::testing::Test {
+protected:
+    void SetUp() override {
+      m_pMemory = new Memory();
+      m_pMemory->Init();
+      m_pMemory->Reset();
+
+      m_pProcessor = new MOS6502Core();
+      m_pProcessor->Init(m_pMemory);
+    }
+
+    void TearDown() override {
+      delete(m_pMemory);
+      delete(m_pProcessor);
+    }
+
+    Memory *m_pMemory;
+    MOS6502Core *m_pProcessor;
+
+};
+
+TEST_F(MOS6502Test, DummyTest) {
+
 }
