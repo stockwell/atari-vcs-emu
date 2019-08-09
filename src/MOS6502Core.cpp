203a16c83883d068e3dce23d55e116f333a0f1e6
Matt Stockwell <matt.stockwell@lifx.co>
2019-08-09 22:49:20 +1000

    MOS6502: Better debug print after each cycle to see how it affects registers

--- src/MOS6502Core.cpp
+++ src/MOS6502Core.cpp
@@ -33,8 +33,10 @@ uint8_t MOS6502Core::FetchOPCode() {
 }
 
 void MOS6502Core::ExecuteOPCode(uint8_t opcode) {
-  printf("Opcode: %s(0x%02X), PC 0x%04X\n", kOPCodeNames[opcode], opcode, m_PC);
+  uint16_t PC = m_PC;
   (this->*m_OPCodes[opcode])();
+  printf("Opcode: %s(0x%02X), PC 0x%04X\n", kOPCodeNames[opcode], opcode, PC);
+  printf("SR: 0x%02X  |  AC: 0x%02X  |  XR: 0x%02X | YR: 0x%02X | SR: 0x%02X | SP: 0x%02X\n", m_SR, m_AC, m_XR, m_YR, m_SR, m_SP);
 }
 
 uint8_t MOS6502Core::Tick() {
