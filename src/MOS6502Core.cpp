6854aa524b22d3fa1e15cfa8d727b942c3240b21
Matt Stockwell <matt.stockwell@lifx.co>
2019-08-15 13:05:15 +1000

    MOS6502: Modify debug print to make it easier to compare operation with Stella in debug mode

--- src/MOS6502Core.cpp
+++ src/MOS6502Core.cpp
@@ -34,17 +34,16 @@ uint8_t MOS6502Core::FetchOPCode() {
 
 void MOS6502Core::ExecuteOPCode(uint8_t opcode) {
   Log("Opcode: %s(0x%02X), PC 0x%04X", kOPCodeNames[opcode], opcode, m_PC);
-  (this->*m_OPCodes[opcode])();
-  Log("SR: 0x%02X  |  AC: 0x%02X  |  XR: 0x%02X | YR: 0x%02X | SP: 0x%02X", m_SR, m_AC, m_XR, m_YR, m_SP);
-  Log("%s%s%s%s%s%s%s",
+  Log("SR: 0x%02X  |  AC: 0x%02X  |  XR: 0x%02X | YR: 0x%02X | SP: 0x%02X [ %s%s%s%s%s%s%s]",
+      m_SR, m_AC, m_XR, m_YR, m_SP,
       m_SR & CARRY ? "C " : "",
       m_SR & ZERO ? "Z " : "",
       m_SR & INTERRUPT ? "I ": "",
       m_SR & DECIMAL ? "D " : "",
       m_SR & BREAK ? "B " : "",
       m_SR & OVERFLOW ? "O ": "",
-      m_SR & NEGATIVE ? "N ": ""
-      );
+      m_SR & NEGATIVE ? "N ": "");
+  (this->*m_OPCodes[opcode])();
 }
 
 uint8_t MOS6502Core::Tick() {
